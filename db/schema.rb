b477d38745dd66547aac106cd1cfc680a1d30426
Ron Gregory <rongregory@pacbell.net>
2015-10-14 21:34:06 -0700

    M:M relationship, HMT. Started some routing.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007171826) do
+ActiveRecord::Schema.define(version: 20151013023006) do
 
   create_table "categories", force: true do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
     t.text     "comment"
     t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "category_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -35,7 +44,9 @@ ActiveRecord::Schema.define(version: 20151007171826) do
   end
 
   create_table "users", force: true do |t|
-    t.string "username"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
