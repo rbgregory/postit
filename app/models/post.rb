fe1bfca8cd9a1a9c161c89e1e0528337e35ae433
Ron Gregory <rongregory@pacbell.net>
2015-12-14 20:26:20 -0800

    Sluggable module.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,63 +1,19 @@
 class Post < ActiveRecord::Base
+  include Voteable
+  include Sluggable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  #polymorphic association.  Rails will "know" that the foreign key is not
-  #post_id on the votes table.  Instead, it's voteable_type and voteable_id
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
-  before_save :generate_slug! #see active record callbacks.
+  #before_save :generate_slug! #see active record callbacks.
 
-  #this goes here, because business logic/data goes in the model, not a helper
-  #not presentation level.  These are data concerns, not presentation level concerns.
-  #Model layer is pure data and data structures.
-  def total_votes
-    up_votes - down_votes
-  end
+  sluggable_column :title
 
-  def up_votes
-    self.votes.where(vote: true).size
-  end
 
-  def down_votes
-    self.votes.where(vote: false).size
-  end
-
-  #this is an override, will return slug instead of default id on the object.
-  def to_param
-    self.slug
-  end
-
-  def generate_slug!
-    the_slug = to_slug(self.title)
-    post = Post.find_by slug: the_slug
-    count = 2
-    while post && post != self
-      the_slug = append_suffix(the_slug, count)
-      post = Post.find_by slug: the_slug
-      count += 1
-    end
-    self.slug = the_slug.downcase
-    #self.title.gsub(" ", "-").downcase -- before we were just substituting - for spaces
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! (/\s*[^A-Za-z0-9]\s*/), '-'
-    str.gsub! (/-+/),"-"
-    str
-  end
 end
---
+++ lib/voteable.rb
@@ -0,0 +1,67 @@
+#Note that this module gets loaded by rails due to the config.autoload_paths += %W(#{config.root}/lib)
+#line in the application.rb file.
+#using concerns
+module Voteable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :voteable
+  end
+
+  def total_votes
+    up_votes - down_votes
+  end
+
+  def up_votes
+    self.votes.where(vote: true).size
+  end
+
+  def down_votes
+    self.votes.where(vote: false).size
+  end
+
+end
+
+=begin
+module Voteable
+  def self.included(base)
+    #meta-programming.  This tells rails to include the InstanceMethods as part of the class
+    #this module is included in.  base.extend tells rails to extend ClassMethods as methods of the
+    #class.  included gets fired when this module is included in a class.
+    base.send(:include, InstanceMethods)
+    base.extend ClassMethods
+    base.class_eval do
+      my_class_method
+    end
+  end
+
+  module InstanceMethods
+    #these methods go here, because business logic/data goes in the model, not a helper...
+    #Not presentation level.  These are data concerns, not presentation level concerns.
+    #Model layer is pure data and data structures.
+    #Note therefore that this module is intended for model layer class(es).
+    def total_votes
+      up_votes - down_votes
+    end
+
+    def up_votes
+      self.votes.where(vote: true).size
+    end
+
+    def down_votes
+      self.votes.where(vote: false).size
+    end
+  end
+
+  module ClassMethods
+    #Placing the has_many here will cause the polymorphic association to occur in any
+    #class that includes this module.
+    def my_class_method
+      #polymorphic association.  Rails will "know" that the foreign key is not
+      #post_id on the votes table.  Instead, it's voteable_type and voteable_id
+      has_many :votes, as: :voteable
+    end
+  end
+
+end
+=end
