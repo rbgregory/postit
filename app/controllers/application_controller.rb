b3902e7fa9172b60edc3e80612709516d65f38c4
Ron Gregory <rongregory@pacbell.net>
2015-12-17 09:03:29 -0800

    Added roles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,14 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  def require_admin
+    access_denied unless logged_in? and current_user.admin?
+  end
+
+  def access_denied
+    flash[:error] = "You don't have permissions for the requested operation."
+    redirect_to root_path
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208040221) do
+ActiveRecord::Schema.define(version: 20151217164509) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20151208040221) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "slug"
+    t.string   "role"
   end
 
   create_table "votes", force: true do |t|
